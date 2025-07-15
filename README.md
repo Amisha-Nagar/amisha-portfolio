5f03fafc5348349f88f0312c5f87c7f61c600c70
Amisha Nagar <amishanagar12@gmail.com>
2025-07-15 18:31:49 +0200

    Your commit message

---
+++ README.md
@@ -0,0 +1,16 @@
+# Summative showcase - Amisha Portfolio (15 July 2025)
+**Chosen theme and purpose:**
+- I created a portfolio website for me as a UX designer and design system specialist. I chose to create a simple website that highlights my key specialities, lists some of my past projects and has some info about myself and my work history.
+- I decided to go for a black and white theme to create an dramatic or edgy feeling, with some doodles here and there to highlight creativity of my role
+
+**The structure of my site:**
+
+
+**Any challenges faced and how I solved them:**
+
+  
+**How I applied accessibility and semantic HTML best practices**
+- In terms of colour, I made sure to use colours that pass AA constrast ratios according to WCAG
+- I also used semantic HTML where possible, like header, main, h1, h2, h3, h4, p, and using alt for images or icon buttons that were necessary to be read out
+- 
+
---
+++ js/script.js
@@ -0,0 +1,53 @@
+
+// Navigation Bar - https://github.com/RhythmusByte/Nav-Bar
+
+const toggleBtn = document.querySelector('.menu');
+const toggleBtnIcon = toggleBtn.querySelector('i');
+const dropDownMenu = document.querySelector('.dropdown-menu');
+
+toggleBtn.onclick = function () {
+  dropDownMenu.classList.toggle('open');
+  const isOpen = dropDownMenu.classList.contains('open');
+  toggleBtnIcon.classList.toggle('fa-bars', !isOpen);
+  toggleBtnIcon.classList.toggle('fa-xmark', isOpen);
+};
+
+
+
+// Typewriter effect - https://css-tricks.com/snippets/css/typewriter-effect/
+var aText = new Array(
+"Hi, I’m Amisha - UX designer and design system specialist."
+);
+var iSpeed = 90; 
+var iIndex = 0; 
+var iArrLength = aText[0].length; 
+var iScrollAt = 20; 
+ 
+var iTextPos = 0; 
+var sContents = ''; 
+var iRow; 
+
+
+function typewriter()
+{
+ sContents =  ' ';
+ iRow = Math.max(0, iIndex-iScrollAt);
+ var destination = document.getElementById("typedtext");
+ 
+ while ( iRow < iIndex ) {
+  sContents += aText[iRow++] + '<br />';
+ }
+ destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos) + "_";
+ if ( iTextPos++ == iArrLength ) {
+  iTextPos = 0;
+  iIndex++;
+  if ( iIndex != aText.length ) {
+   iArrLength = aText[iIndex].length;
+   setTimeout("typewriter()", 500);
+  }
+ } else {
+  setTimeout("typewriter()", iSpeed);
+ }
+}
+
+typewriter();
